8554dd05142349208aeeebca73c95740630f7a6b
Vishal3488562 <107636738+Vishal3488562@users.noreply.github.com>
2025-03-26 11:13:27 +0530

    Update DateRangeViewSection.jsx

--- app/components/admin/DateRangeViewSection.jsx
+++ app/components/admin/DateRangeViewSection.jsx
@@ -16,20 +16,21 @@ export default function DateRangeViewSection({
   dredgers,
 }) {
   return (
-    // <div className="flex flex-col md:flex-row text-sm md:text-xl justify-between items-center mb-6 bg-white p-4 rounded-lg shadow w-full">
-    //   <div className="w-full md:w-auto mb-4 md:mb-0">
+ 
+    // <div className="flex flex-col md:flex-row sm:text-xs md:text-xl justify-between items-center mb-4 bg-white p-3 rounded-lg shadow w-full">
+    //   <div className="w-full md:w-auto mb-2 md:mb-0 flex flex-wrap items-center">
     //     {/* Date input */}
-    //     <label className="mr-2 md:font-semibold">Date:</label>
+    //     <label className="mr-1 font-semibold">Date:</label>
     //     <input
     //       type="date"
-    //       className="p-2 border rounded-md w-1/3 md:w-auto focus:outline-none focus:ring-2 focus:ring-blue-500"
+    //       className="p-1 border rounded-md w-[7.2rem] md:w-auto focus:outline-none focus:ring-2 focus:ring-blue-500"
     //       value={date}
     //       onChange={(e) => setDate(e.target.value)}
     //     />
     //     {/* Dredger selection */}
-    //     <label className="ml-6 mr-2 font-semibold">Dredger:</label>
+    //     <label className="ml-4 mr-1 font-semibold">Dredger:</label>
     //     <select
-    //       className="p-2 border rounded-md w-[5rem] md:w-auto focus:outline-none focus:ring-2 focus:ring-blue-500"
+    //       className="p-1 border rounded-md w-[4.5rem] md:w-auto focus:outline-none focus:ring-2 focus:ring-blue-500"
     //       value={selectedDredger}
     //       onChange={(e) => setSelectedDredger(e.target.value)}
     //     >
@@ -41,31 +42,29 @@ export default function DateRangeViewSection({
     //     </select>
     //   </div>
 
-    //   <div className="w-full md:w-auto mb-4 md:mb-0">
-    //     <div className="md:flex md:items-center">
-    //       {/* Date range inputs */}
-    //       <label className="mr-2 font-semibold">Range:</label>
-    //       <input
-    //         type="date"
-    //         className="p-2 border rounded-md w-[8rem] focus:outline-none focus:ring-2 focus:ring-blue-500"
-    //         value={startDate}
-    //         onChange={(e) => setStartDate(e.target.value)}
-    //       />
-    //       <label className="ml-4 mr-2 font-semibold">to:</label>
-    //       <input
-    //         type="date"
-    //         className="p-2 border rounded-md w-[8rem] focus:outline-none focus:ring-2 focus:ring-blue-500"
-    //         value={endDate}
-    //         onChange={(e) => setEndDate(e.target.value)}
-    //       />
-    //     </div>
+    //   <div className="w-full md:w-auto mb-2 md:mb-0 flex flex-wrap items-center">
+    //     {/* Date range inputs */}
+    //     <label className="mr-1 font-semibold">Range:</label>
+    //     <input
+    //       type="date"
+    //       className="p-1 border rounded-md md:w-[11rem] w-[7rem] focus:outline-none focus:ring-2 focus:ring-blue-500"
+    //       value={startDate}
+    //       onChange={(e) => setStartDate(e.target.value)}
+    //     />
+    //     <label className="ml-2 mr-1 font-semibold">to:</label>
+    //     <input
+    //       type="date"
+    //       className="p-1 border rounded-md md:w-[11rem] w-[7rem]  focus:outline-none focus:ring-2 focus:ring-blue-500"
+    //       value={endDate}
+    //       onChange={(e) => setEndDate(e.target.value)}
+    //     />
     //   </div>
 
-    //   <div className="w-full md:w-auto">
+    //   <div className="w-full md:w-auto flex flex-wrap items-center">
     //     {/* View selection */}
-    //     <label className="mr-2 font-semibold">View:</label>
+    //     <label className="mr-1 font-semibold">View:</label>
     //     <select
-    //       className="p-2 border rounded-md w-1/3 md:w-auto focus:ring-2 focus:ring-blue-500"
+    //       className="p-1 border rounded-md w-[5rem] md:w-auto focus:ring-2 focus:ring-blue-500"
     //       value={selectedView}
     //       onChange={(e) => setSelectedView(e.target.value)}
     //     >
@@ -74,7 +73,8 @@ export default function DateRangeViewSection({
     //     </select>
     //   </div>
     // </div>
-    <div className="flex flex-col md:flex-row sm:text-xs md:text-xl justify-between items-center mb-4 bg-white p-3 rounded-lg shadow w-full">
+
+        <div className="flex flex-col md:flex-row text-xs md:text-xl justify-between items-center mb-4 bg-white p-3 rounded-lg shadow w-full">
       <div className="w-full md:w-auto mb-2 md:mb-0 flex flex-wrap items-center">
         {/* Date input */}
         <label className="mr-1 font-semibold">Date:</label>
@@ -87,7 +87,7 @@ export default function DateRangeViewSection({
         {/* Dredger selection */}
         <label className="ml-4 mr-1 font-semibold">Dredger:</label>
         <select
-          className="p-1 border rounded-md w-[4.5rem] md:w-auto focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className="p-1 border rounded-md w-[4em] md:w-auto focus:outline-none focus:ring-2 focus:ring-blue-500"
           value={selectedDredger}
           onChange={(e) => setSelectedDredger(e.target.value)}
         >
@@ -104,7 +104,7 @@ export default function DateRangeViewSection({
         <label className="mr-1 font-semibold">Range:</label>
         <input
           type="date"
-          className="p-1 border rounded-md md:w-[11rem] w-[7rem] focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className="p-1 border rounded-md md:w-[11rem] w-[6rem] focus:outline-none focus:ring-2 focus:ring-blue-500"
           value={startDate}
           onChange={(e) => setStartDate(e.target.value)}
         />
